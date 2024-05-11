4723c5ed17b7b83d79d9f4d6991223adfb5f5dde
The-Slick-Nick <96804604+The-Slick-Nick@users.noreply.github.com>
2024-05-11 15:09:11 -0500

    feat: Add more coloring options and flavor to boxes

--- box_creation.js
+++ box_creation.js
@@ -2,34 +2,58 @@
  * This file is intended to create a 16x16 grid of cells within a high-level container div
  **/
 
-
+const STATIC_GREEN_VAL = 128;
+const GRID_SIZE = 16;
+const BOX_INDEX_COLOR_SCALE = 256 / GRID_SIZE;
 const mainContainer = document.querySelector(".box-container");
 
-function handleHoverEnter(event) {
-    /* handle hover leaving element */
-    event.target.classList.add("is-hovered");
-}
+function rgb(r, g, b) {
+    /* return the hex string associated with a given combination of r, g,
+     * and b number values
+     */
+    if (r < 0 || r > 255) {
+        throw Error(`r value not in defined range. got ${r}`);
+    }
 
-function handleHoverLeave(event) {
-    /* handle hover entering element */
-    event.target.classList.remove("is-hovered");
-}
+    if (g < 0 || g > 255) {
+        throw Error(`g value not in defined range. got ${g}`);
+    }
 
+    if (b < 0 || b > 255) {
+        throw Error(`b value not in defined range. got ${b}`);
+    }
+
+    let toReturn = "#";
+    toReturn += r.toString(16).padStart(2, "0");
+    toReturn += g.toString(16).padStart(2, "0");
+    toReturn += b.toString(16).padStart(2, "0");
+    return toReturn;
+}
 /* 16 x 16 grid */
 /* first make 16 row containers */
 
 let rowContainer;
 let box;
-for (let i = 0; i < 16; i++) {
+for (let rowNum = 1; rowNum <= GRID_SIZE; rowNum++) {
     rowContainer = document.createElement("div"); 
     rowContainer.classList.add("row-container");
     mainContainer.appendChild(rowContainer);
 
-    for (let j = 0; j < 16; j++) {
+    for (let colNum = 1; colNum <= GRID_SIZE; colNum++) {
         box = document.createElement("div");
         box.classList.add("box");
-        box.addEventListener("mouseenter", handleHoverEnter);
-        box.addEventListener("mouseleave", handleHoverLeave);
+
+        let boxEventListener = function(event) {
+            event.target.style["background-color"] = rgb(
+                (rowNum - 1) * BOX_INDEX_COLOR_SCALE,
+                STATIC_GREEN_VAL,
+                (colNum - 1) * BOX_INDEX_COLOR_SCALE
+            );
+        }
+
+        /* mouse over -> color || click -> white */
+        box.addEventListener("mouseenter", boxEventListener);
+        box.addEventListener("mousedown", (event) => event.target.style["background-color"] = "white");
         rowContainer.appendChild(box);
     }
 }
